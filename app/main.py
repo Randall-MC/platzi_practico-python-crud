28d29ef62cc63a9bb41e5b711e5afd5987d68222
Randall-ATG15 <randallmarcu@gmial.com>
2023-12-14 10:12:38 -0600

    Refactor client data structure and update client functions

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
 import sys
 
-clients = ['Pablo', 'Ricardo']
+clients = [
+    {
+        'name': 'Pablo',
+        'company': 'Google',
+        'email': 'pablo@google.com',
+        'position': 'Software engineer',
+    },
+    {
+        'name': 'Ricardo',
+        'company': 'Facebook',
+        'email': 'ricardo@facebook.com',
+        'position': 'Data engineer',
+    }
+]
 
 def welcome():
     print('¡ Welcome to Platzi Ventas !',
@@ -10,48 +23,45 @@ def welcome():
           '[U] Update client',
           '[D] Delete client',
           '[L] List clients',
-          '[S] Search client',
           '>>>', sep='\n', end=' ')
 
-def create_client(client_name):
+def create_client(client):
     # Take global variable clients to use it
     global clients
 
-    if client_name not in clients:
-        # Add element client_name to the list
-        clients.append(client_name)
+    if client not in clients:
+        # Add element client to the list
+        clients.append(client)
     else:
-        print(f"The client with the name {client_name} already exists in the database.")
+        print(f"The client with the name {client} already exists in the database.")
      
 
-def update_client(client_name, updated_name):
+def update_client(client_name):
     global clients
 
-    if client_name in clients:
-        index = clients.index(client_name)
-        clients[index] = updated_name
+    for client in clients:
+        if client['name'] == client_name:
+            new_name = input('What is the updated client name? ')
+            client['name'] = new_name
+            break
     else:
-        print(f'The client with the name {client_name} is not exists in our database.')
+        print(f'The client with the name {client_name} does not exist in our database.')
 
 
 def delete_client(client_name):
     global clients
 
-    if client_name in clients:
-        clients.remove(client_name)
+    for client in clients:
+        if client['name'] == client_name:
+            clients.remove(client)
+            break
     else:
         print(f'The client with the name {client_name} is not exists in our database.')
 
 
-def search_client(client_name):
-    global clients
-
-    return client_name in clients
-
-
 def list_clients():
     for idx, client in enumerate(clients):
-        print(f'{idx}: {client}')
+        print(f'{idx}: {client["name"]}, {client["company"]}, {client["email"]}, {client["position"]}')
 
 
 def _get_client_name():
@@ -67,7 +77,16 @@ def _get_client_name():
     if not client_name:
         sys.exit()
 
-    return input('What is the client name? ')
+    return client_name
+
+
+def _get_client_field(field_name):
+    field = None
+
+    while not field:
+        field = input(f'What is the client {field_name}? ')
+
+    return field
 
 
 if __name__ == '__main__':
@@ -77,13 +96,18 @@ if __name__ == '__main__':
     command = command.upper()
 
     if command == 'C':
-        client_name = _get_client_name()
-        create_client(client_name)
+        # Create a dictionary with the client data
+        client = {
+            'name': _get_client_field('name'),
+            'company': _get_client_field('company'),
+            'email': _get_client_field('email'),
+            'position': _get_client_field('position'),
+        }
+        create_client(client)
         list_clients()
     elif command == 'U':
         client_name = _get_client_name()
-        updated_name = input('What is the new client name? ')
-        update_client(client_name, updated_name)
+        update_client(client_name)
         list_clients()
     elif command == 'D':
         client_name = _get_client_name()
@@ -91,13 +115,6 @@ if __name__ == '__main__':
         list_clients()
     elif command == 'L':
         list_clients()
-    elif command == 'S':
-        client_name = _get_client_name()
-        found = search_client(client_name)
-        if found:
-            print(f'The client {client_name} is in the client\'s list')
-        else:
-            print(f'The client {client_name} is not in our client\'s list')
     else:
         print('Invalid command')
 
